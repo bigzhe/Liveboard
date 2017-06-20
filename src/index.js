8e865365688f11ee6f5371b75c562d7478a230cf
Haozhe <imhaozhe@gmail.com>
2017-06-20 23:40:32 +0100

    private ip

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ injectTapEventPlugin();
 
 // const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-let socket = io('http://localhost:3000')
+let socket = io('http://10.130.3.153:3000')
 let socketIoMiddleware = createSocketIoMiddleware(socket, "server/")
 
 const store = applyMiddleware(socketIoMiddleware, thunk)(createStore)(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
